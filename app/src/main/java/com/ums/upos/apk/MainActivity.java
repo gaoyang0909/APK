e4b53950cb885411dc82f5373097841484edf397
gaoyang <1034488794@qq.com>
2019-10-11 19:06:17 +0800

    第二次提交测试

--- app/src/main/java/com/ums/upos/apk/MainActivity.java
+++ app/src/main/java/com/ums/upos/apk/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   Integer  a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
